1a76e13872721d3b42127e9b08438d1d3cbe6212
hegyitaho <hegyitaho@gmail.com>
2019-12-23 15:12:32 +0100

    deploy functions individually

--- src/express/server.ts
+++ src/express/server.ts
@@ -2,7 +2,6 @@
 'use strict'
 import express, {Router} from 'express'
 import {join} from 'path'
-import serverless from 'serverless-http'
 import {json} from 'body-parser'
 import {getSiteMenu} from '../scraping/beresalexandra/converted-menu'
 import {FoodData} from '../scraping/beresalexandra/utils/FoodData'
@@ -18,30 +17,10 @@ router.get('/', (req, res) => {
 })
 
 const beresalexandra = Router()
-router.use('/beresalexandra', beresalexandra)
-beresalexandra.get('/', (req, res) => res.send('hello!!!!!!!!!!!!!!!'))
-beresalexandra.get('/current', async (req, res) => res.json(await beresalexandraCurrentMenu()))
-beresalexandra.get('/next', async (req, res) => res.json(await beresalexandraNextMenu()))
 beresalexandra.get('/dummy', async (req, res) => res.json(await dummyFoodDataForAWeek()))
 
 app.use(json())
-app.use('/.netlify/functions', router)  // path must route to lambda
+// app.use('/.netlify/functions', router)  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(join(__dirname, './index.html')))
 
-export const handler = serverless(app)
-
-let currentMenu, nextMenu: FoodData[]
-
-async function beresalexandraCurrentMenu() {
-  if (!currentMenu) {
-    currentMenu = await getSiteMenu()
-  }
-  return currentMenu
-}
-
-async function beresalexandraNextMenu() {
-  if (!nextMenu) {
-    nextMenu = await getSiteMenu({next: true})
-  }
-  return nextMenu
-}
+// export const handler = serverless(app)
